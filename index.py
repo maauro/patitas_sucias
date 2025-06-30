e07fe6e7b8be6bf152b542bcafac37fcb308b323
Mauro Caneo <mauricio.caneo@latam.com>
2025-06-30 16:20:46 -0400

    fix: Markup

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, flash, session, Markup  # type: ignore
+from flask import Flask, render_template, request, jsonify, redirect, url_for, flash, session  # type: ignore
 from flask_sqlalchemy import SQLAlchemy  # type: ignore
 from datetime import datetime
 from zoneinfo import ZoneInfo
